5876ad5960b2daec9c7cf2b8f810bdaf9ef2ae3d
bappa <iftakharul.alam@live.com>
2015-07-17 18:40:59 +0600

    send email with style

--- resources/views/emails/welcome.blade.php
+++ resources/views/emails/welcome.blade.php
@@ -4,9 +4,9 @@
     <meta charset="utf-8">
 </head>
 <body>
-<h2>Learning Laravel!</h2>
-
-<div>
+<h2 style="font-weight: bold;text-decoration: underline;color: #d43f3a;text-align: center">Learning Laravel!</h2>
+<hr/>
+<div style="padding: 10px;border: 1px dashed deepskyblue">
     Welcome to {!! $name !!} website!
 </div>
 
